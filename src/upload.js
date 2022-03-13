e5c6fbd5a4ef3fbd7798088f63244a254650744f
sumanthreddyc <sumanth.challaram@gmail.com>
2022-03-13 12:36:33 +0530

    Finished taking screenshots and uploading to s3

--- src/upload.js
+++ src/upload.js
@@ -1,10 +1,7 @@
 "use strict";
-
 const AWS = require("aws-sdk");
 const { v4: uuidv4 } = require("uuid");
 const s3 = new AWS.S3();
-const width = 200;
-const height = 200;
 const imageType = "image/png";
 const bucket = process.env.Bucket;
 const chromium = require("chrome-aws-lambda");
@@ -13,14 +10,10 @@ const fs = require("fs");
 module.exports.handler = async (event, context, callback) => {
   let requestBody = JSON.parse(event.body);
   let domainUrl = requestBody.domainUrl;
-  let result = null;
+  let domainName = requestBody.domainName;
   let browser = null;
   let objectId = uuidv4();
-  let objectKey = `resize-${width}x${height}-${objectId}.png`;
-  // await page.goto("https://www.scrapehero.com/");
-  // await page.screenshot({ path: "./image.jpg", type: "jpeg" });
-  // await page.close();
-  // await browser.close();
+  let objectKey = `${domainName}-${objectId}.png`;
 
   browser = await chromium.puppeteer.launch({
     args: chromium.args,
@@ -31,28 +24,18 @@ module.exports.handler = async (event, context, callback) => {
   });
 
   let page = await browser.newPage();
-  console.log("domainUrl:", domainUrl);
-
-  await page.goto(domainUrl || "https://example.com");
-  await page.screenshot({ path: "/tmp/image.jpg", type: "jpeg" });
-  console.log("screenshot captured");
+  await page.goto(domainUrl || "https://example.com", {
+    waitUntil: "networkidle0",
+  });
+  const buffer = await page.screenshot();
   await page.close();
   await browser.close();
 
-  const fileName = "/tmp/image.jpg";
-
-  fs.readFile(fileName, (err, data) => {
-    if (err) throw err;
-    console.log("data:", data);
-    uploadToS3(data, objectKey)
-      .then(function (response) {
-        console.log(`Image ${objectKey} was uploaed and resized`);
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(objectKey),
-        });
-      })
-      .catch((error) => console.log(error));
+  await uploadToS3(buffer, objectKey).then(function (response) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(objectKey),
+    });
   });
 };
 
